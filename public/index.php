d5e4fffb61a3a0450e0dde146ba315d64de9b85f
jakimfett <jakimfett@picraft.com>
2014-05-16 13:32:04 -0700

    Fixing include locations

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 require '../MCServerStatus/Minecraft/Server.php';
 require '../MCServerStatus/Minecraft/Stats.php';
-require_once 'includes/controller.php';
-require_once 'includes/header.php';
-require_once 'includes/view.php';
-require_once 'includes/footer.php';
+require_once '../protected/includes/controller.php';
+require_once '../protected/includes/header.php';
+require_once '../protected/includes/view.php';
+require_once '../protected/includes/footer.php';
